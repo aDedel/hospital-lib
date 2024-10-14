f468f7fdce310d353f6640976fbe52d21c247e7d
Alexandre DEL VECCHIO <alexdelvecchio@gmail.com>
2024-10-14 20:09:55 +0200

    feat: update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
+            "@/object-curly-spacing": ["error", "always"],
+        },
+    },
+    {
+        ignores: ["dist/*"],
+    },
+);
